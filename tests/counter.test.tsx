f18e67d7e7321906a4f886aaeb3d3fe1f76d13ec
prodipsb <prodip5080@gmail.com>
2024-09-21 02:08:18 +0600

    npm test

--- tests/counter.test.tsx
+++ tests/counter.test.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import {render} from '@testing-library/react'
+import * as React from 'react'
+import { render } from '@testing-library/react'
 
 import 'jest-canvas-mock'
 
-import {MyCounter} from '../src';
+import { MyCounter } from '../src'
 
 describe('Common render', () => {
-    it('render without crashing', ()=>{
-        render(<MyCounter/>)
-    })
+  it('render without crashing', () => {
+    render(<MyCounter />)
+  })
 })
